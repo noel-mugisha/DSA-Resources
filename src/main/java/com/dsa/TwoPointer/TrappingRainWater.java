8ead8fcf531a1eb3a243994401ecba08f8d2b23b
Noel <noelmugisha332@gmail.com>
2026-06-08 13:51:12 +0200

    Trapping rain water

---
+++ src/main/java/com/dsa/TwoPointer/TrappingRainWater.java
@@ -0,0 +1,23 @@
+package com.dsa.TwoPointer;
+
+public class TrappingRainWater {
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int waterTrapped = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                waterTrapped += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = Math.max(rightMax, height[right]);
+                waterTrapped += rightMax - height[right];
+                right--;
+            }
+        }
+
+        return waterTrapped;
+    }
+}
